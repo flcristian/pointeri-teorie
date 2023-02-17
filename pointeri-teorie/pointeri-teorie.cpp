25a6927695e15922fdd856e67d6551c22b4fe907
Florescu Cristian <qflorescucristian@gmail.com>
2023-02-17 13:05:49 +0200

    Tema pentru 17.02

--- pointeri-teorie/pointeri-teorie.cpp
+++ pointeri-teorie/pointeri-teorie.cpp
@@ -1,10 +1,6 @@
-#include "exercitii.h"
+#include "tema-pentru-17-02.h"
 
 int main()
 {	
-	int x[5] = { 10,23,8,92,2 };
-	int m, n;
-	maxAndMinPrinAdresa(x, 5, &m, &n);
-	cout << "Max = " << m << endl;
-	cout << "Min = " << n << endl;
+	problema10();
 }  
